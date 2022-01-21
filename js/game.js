21944a9c481c4f982d56e2e778b4e949b657cc5d
Doan Tran <doan7tran@gmail.com>
2022-01-21 16:22:40 +0100

    creating game structure with classes and board

---
+++ js/game.js
@@ -0,0 +1,51 @@
+class Game {
+    constructor(){
+
+    }
+
+start (){}
+
+setInterval (()=>{
+
+})
+
+addEventListeners(){}
+
+createDomElm(){}
+
+drawDomElm(){}
+
+collisionObstacles (){}
+
+collectingGoods() {}
+
+
+}
+
+
+class Player{
+    constructor (){
+
+    }
+moveLeft(){}
+
+moveRight(){}
+
+}
+
+
+class Obstacles {
+    constructor(){}
+
+moveDown(){}
+
+removeObstacle (){}
+}
+
+class Goods {  // inherit from obstacle
+    constructor(){}
+
+}
+
+const game = new Game();
+game.start();
